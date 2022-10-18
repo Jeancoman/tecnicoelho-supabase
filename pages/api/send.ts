759afd099ad5ca3d7930d0721bc506ab11d57650
Jean Bolívar <75143878+Jeancoman@users.noreply.github.com>
2022-10-18 16:25:28 -0400

    Update send.ts

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,16 +1,16 @@
 import sendgrid from "@sendgrid/mail";
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-sendgrid.setApiKey("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");
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY!);
 
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    await sendgrid.send({
-      to: "jeanfranbg@gmail.com", // Your email where you'll receive emails
-      from: "tecnicoelhocliente@gmail.com", // your website email address here
-      subject: `${req.body.asunto}`,
-      text: `${req.body.mensaje}`,
-      html: ` </div>
+
+  await sendgrid.send({
+    to: "jeanfranbg@gmail.com",
+    from: "tecnicoelhocliente@gmail.com",
+    subject: `${req.body.asunto}`,
+    text: `${req.body.mensaje}`,
+    html: ` </div>
       <div class="container" style="margin-left: 20px;margin-right: 20px;">
       <h3>Tienes un mensaje de ${req.body.nombre}, su correo electronico es : ✉️${req.body.email} </h3>
       <h3>Y su número telefonico es : 📱${req.body.telefono} </h3>
@@ -19,13 +19,9 @@ async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
       <p>${req.body.mensaje}</p>
       <br>
       </div>`,
-    });
-    console.log("Enviado")
-  } catch (error: any) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
-  }
+  });
 
-  return res.status(200).json({ error: "" });
+  return res.status(200).json({});
 }
 
 export default sendEmail;
