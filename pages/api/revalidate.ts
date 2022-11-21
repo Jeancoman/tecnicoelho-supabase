84da798f0c5533637c0c53059bfd04d2e42cdb23
Jeancoman <jeanfranbg@gmail.com>
2022-11-21 00:15:01 -0400

    A

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -6,13 +6,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const path = req.body?.table === "producto" ? "productos" : "publicaciones";
 
-    if (path === "productos") {
-        await res.revalidate("/produtos");
-    } else {
-        await res.revalidate("/publicaciones");
-    }
+    await res.revalidate(`/${path}`);
 
     const pathToRevalidate = `/${path}/${req.body?.record?.id || req.body?.old_record?.id}`;
+
+    console.log(pathToRevalidate);
+    console.log(path);
+    console.log(req.body);
 
     await res.revalidate(pathToRevalidate);
 
